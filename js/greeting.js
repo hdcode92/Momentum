3080175714c0a7a66d087abd46ed58e0e7052b99
HDCodes <hundazel@gmail.com>
2022-02-20 16:59:56 +0900

    java script

--- js/greeting.js
+++ js/greeting.js
@@ -4,27 +4,29 @@ const greeting = document.querySelector("#greeting");
 const inputbox = document.querySelector(".inputbox");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username"
+const tdf = document.getElementById("todo-form");
 
 
 function onLoginSubmit(event){
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username)
     paintGreetings(username);}
 
 function paintGreetings(username){
-    greeting.innerText = `Hello ${username}`;
+    greeting.innerText = `Hello, ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    tdf.classList.remove(HIDDEN_CLASSNAME);
     inputbox.remove();
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if(savedUsername === null){
-    greeting.innerText = "what is your name?";
+    greeting.innerText = "What is your name?";
     loginForm.classList.remove(HIDDEN_CLASSNAME);
+    tdf.classList.add(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
     } else {
     paintGreetings(savedUsername);
